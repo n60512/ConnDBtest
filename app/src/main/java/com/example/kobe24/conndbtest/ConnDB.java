02f82eb578fac444d59c120c264276896985a050
n60512 <n60512@gmail.com>
2018-08-25 00:16:54 +0800

    0824

--- app/src/main/java/com/example/kobe24/conndbtest/ConnDB.java
+++ app/src/main/java/com/example/kobe24/conndbtest/ConnDB.java
@@ -1,92 +1,227 @@
 package com.example.kobe24.conndbtest;
 
+import android.graphics.Bitmap;
+import android.util.Base64;
 import android.util.Log;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.HttpEntity;
 import org.apache.http.util.EntityUtils;
-
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+
+import java.io.ByteArrayOutputStream;
 import java.util.*;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.json.*;
 
 public class ConnDB {
 
-    public ConnDB(){
-        //String link = "http://140.122.91.218/test/posttest.php";
-        drawing("xbox","user1");
-    }
+    private String database = null;
+    private String webRequest = null;      //  web request;type:json
 
-    private void  jsonParse(String content){
-        JSONObject obj;
-        try {
-            String tmp = content;//successful error
-            obj = new JSONObject(tmp);
-            Object jsonOb = obj.getJSONObject("drawing");
-            Log.d("json",jsonOb.toString());
-        }catch(Exception e){
+    /**
+     * 處理遊戲 drawing,drawingmult 建構子
+     *
+     * @param database
+     * @param content
+     * @param crtuser
+     */
+    public ConnDB(String database, String content, String crtuser, Bitmap image) {
+        this.database = database;
+        switch (this.database) {
+            case "drawing":
+                draw(content, crtuser, toBase64(image));
+                break;
+            case "drawingmult":
+                draw(content, crtuser, toBase64(image));
+                break;
+            default:
+                ;
         }
     }
 
-    /// <summary>
-    /// connect drawing db
-    /// </summary>
-    /// <param name="Name">pictureName</param>
-    /// <param name="crtuser">userName</param>
-    private void  drawing(String Name,String crtuser){
-        HttpClient client = new DefaultHttpClient();
-        HttpPost request = new HttpPost("http://140.122.91.218/test/drawing.php");
+    /**
+     * oneimage 建構子
+     *
+     * @param database
+     * @param content
+     * @param crtuser
+     * @param imageID
+     */
+    public ConnDB(String database, String content, String crtuser, String imageID) {
+        this.database = database;
 
-        String webRequest = null;      //  web request;type:json
+        if (this.database.equals("oneimage")) {
+            oneimage(content, crtuser, imageID);
+        } else {
+        }
+
+    }
+
+    /**
+     * 處理遊戲 association 建構子
+     *
+     * @param database
+     * @param description
+     * @param chosenList
+     * @param crtuser
+     */
+    public ConnDB(String database, String description, String[] chosenList, String crtuser) {
+        this.database = database;
+
+        int index = 0;
+        String tmpText = "";
+        for (String option : chosenList)
+            tmpText = tmpText + "'chosen" + Integer.toString(index = index + 1) + "','" + option + "',";
+
+        String chosenContent = "column_create(" + tmpText.substring(0, tmpText.length() - 1) + ")";     // column_create('chosen1','0','chosen2','0' ...)
+        //showMessage("test", tmpText); for test
+
+        if (this.database.equals("association")) {
+            association(description, chosenContent, crtuser);
+        } else {
+        }
+    }
+
+    /**
+     * @param Name    pictureName
+     * @param crtuser userName
+     */
+    private void draw(String Name, String crtuser, String base64img) {
+
+        JSONObject obj;
+        //Object jsonOb = null;
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost request = new HttpPost("http://140.122.91.218/thinkingapp/connDB/drawing.php");
+
         List<NameValuePair> params = new ArrayList<NameValuePair>();
 
-        params.add(new BasicNameValuePair("Name",Name));
-        params.add(new BasicNameValuePair("crtuser",crtuser));
+        params.add(new BasicNameValuePair("database", this.database));
+        params.add(new BasicNameValuePair("Name", Name));
+        params.add(new BasicNameValuePair("base64img", base64img));
+        params.add(new BasicNameValuePair("crtuser", crtuser));
+
 
         try {
-            request.setEntity(new UrlEncodedFormEntity(params));    //  set post params
+
+            request.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));    //  set post params
             HttpResponse response = client.execute(request);        //  get web response
             HttpEntity resEntity = response.getEntity();
-            webRequest = EntityUtils.toString(resEntity);
+            webRequest = EntityUtils.toString(resEntity);             //  取得網頁 REQUEST
 
-            Log.d("webRequest",webRequest);
+            showMessage("webRequest", webRequest);
 
-            jsonParse(webRequest);  //  json parse
-            //EntityUtils.consume(resEntity);
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d("IOException", e.getMessage());
+            obj = new JSONObject(this.webRequest);  // parse web request
+            String webResponse = obj.getJSONObject(this.database).getString("response");
+            showMessage(this.database, webResponse);
+
+        } catch (java.io.IOException e) {
+            showMessage("IOException", e.getMessage());
+        } catch (org.json.JSONException e) {
+            showMessage("JSON Error", e.getMessage());
         }
-}
+    }
 
-    private void connWeb(String link){
+    /**
+     * oneimage 建構子
+     *
+     * @param Data
+     * @param crtuser
+     * @param imageID
+     */
+    private void oneimage(String Data, String crtuser, String imageID) {
+
+        JSONObject obj;
+        //Object jsonOb = null;
 
         HttpClient client = new DefaultHttpClient();
-        //HttpGet request = new HttpGet(link);
-        HttpPost request = new HttpPost(link);
+        HttpPost request = new HttpPost("http://140.122.91.218/thinkingapp/connDB/oneimage.php");
+
 
         List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("user","John"));
-        params.add(new BasicNameValuePair("age","23"));
+
+        params.add(new BasicNameValuePair("database", this.database));
+        params.add(new BasicNameValuePair("Data", Data));
+        params.add(new BasicNameValuePair("imageID", imageID));
+        params.add(new BasicNameValuePair("crtuser", crtuser));
 
         try {
-            request.setEntity(new UrlEncodedFormEntity(params));    //set params
 
-            HttpResponse response = client.execute(request);
+            request.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));    //  set post params
+            HttpResponse response = client.execute(request);        //  get web response
+
             HttpEntity resEntity = response.getEntity();
-            String webRequest = EntityUtils.toString(resEntity);
-            Log.d("webRequest",webRequest);
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d("IOException", e.getMessage());
-        }
+            webRequest = EntityUtils.toString(resEntity);             //  取得網頁 REQUEST
 
+            //showMessage("webRequest", webRequest);
+
+            obj = new JSONObject(this.webRequest);  // parse web request
+            String webResponse = obj.getJSONObject(this.database).getString("response");
+            showMessage(this.database, webResponse);
+
+        } catch (java.io.IOException e) {
+            showMessage("IOException", e.getMessage());
+        } catch (org.json.JSONException e) {
+            showMessage("JSON Error", e.getMessage());
+        }
     }
+
+    /**
+     * Game association connection
+     *
+     * @param description
+     * @param chosenContent
+     * @param crtuser
+     */
+    private void association(String description, String chosenContent, String crtuser) {
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost request = new HttpPost("http://140.122.91.218/thinkingapp/connDB/association.php");
+
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+        params.add(new BasicNameValuePair("Description", description));
+        params.add(new BasicNameValuePair("ChosenContent", chosenContent));
+        params.add(new BasicNameValuePair("crtuser", crtuser));
+
+        try {
+            request.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));    //  set post params
+            HttpResponse response = client.execute(request);        //  get web response
+            HttpEntity resEntity = response.getEntity();
+            webRequest = EntityUtils.toString(resEntity);             //  取得網頁 REQUEST
+
+            showMessage("association webRequest", webRequest);
+
+        } catch (java.io.IOException e) {
+            showMessage("IOException", e.getMessage());
+        }
+    }
+
+    /**
+     * convert bitmap to base64
+     *
+     * @param bitmap
+     * @return
+     */
+    public String toBase64(Bitmap bitmap) {
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        bitmap.recycle();   //  recycle resource
+
+        return android.util.Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+    private void showMessage(String title, String content) {
+        Log.d(title, content);
+    }
+
 }
--- app/src/main/java/com/example/kobe24/conndbtest/MainActivity.java
+++ app/src/main/java/com/example/kobe24/conndbtest/MainActivity.java
@@ -1,9 +1,28 @@
 package com.example.kobe24.conndbtest;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.*;
+
+import com.squareup.picasso.Picasso;
+
+import android.graphics.drawable.Drawable;
+import android.util.Base64;
+import android.util.Log;
+import android.graphics.BitmapFactory;
+
+import java.io.ByteArrayOutputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    Bitmap myBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,12 +30,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-        // Android 4.0 之后不能在主线程中请求HTTP请求
-        new Thread(new Runnable(){
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                ConnDB test = new ConnDB();
+
+                String chosen = "";
+                switch (chosen) {
+                    case "drawing":
+                        try {
+                            URL url = new URL("http://140.122.91.218/thinkingapp/associationrulestest/image/2.png");
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                            connection.setDoInput(true);
+                            connection.connect();
+                            InputStream input = connection.getInputStream();
+                            myBitmap = BitmapFactory.decodeStream(input);
+
+                        } catch (IOException e) {
+                            // Log exception
+                        } finally {
+                            ConnDB conn1 = new ConnDB("drawing", "公雞", "user1", myBitmap);
+                            myBitmap.recycle();   //  recycle resource
+                        }
+                        break;
+                    case "drawingmult":
+                        break;
+                    case "oneimage":
+                        ConnDB conn1 = new ConnDB("oneimage", "同心心圓", "user1", "34");
+                        break;
+                    case "association":
+                        String[] testArr = {"55", "15", "23"};
+                        ConnDB conn2 = new ConnDB("association", "TEST0820", testArr, "user1");
+                        break;
+                    default:
+                        break;
+
+                }
             }
         }).start();
 
